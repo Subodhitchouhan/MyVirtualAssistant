2513089e50612f2fb74cfd865f1460953d001319
Subodhit Chouhan <chouhansubodhit@gmail.com>
2025-08-04 01:44:49 +0530

    addd

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"ourvirtualassistant.netlify.app",
+    origin:"https://ourvirtualassistant.netlify.app",
     credentials:true
 }))
 const port=process.env.PORT || 5000
