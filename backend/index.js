e6190980da32e4beb663b913c7efeea1da308bfd
Subodhit Chouhan <chouhansubodhit@gmail.com>
2025-08-04 00:59:35 +0530

    add frontend url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://my-virtual-assistant-7u1rcipbt-subodhits-projects.vercel.app/signup",
     credentials:true
 }))
 const port=process.env.PORT || 5000
