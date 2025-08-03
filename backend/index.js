ac00d63df07af6a241e44b47452a02e115309ebd
Subodhit Chouhan <chouhansubodhit@gmail.com>
2025-08-04 01:34:46 +0530

    add netlify link

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://my-virtual-assistant-nu.vercel.app",
+    origin:"ourvirtualassistant.netlify.app",
     credentials:true
 }))
 const port=process.env.PORT || 5000
